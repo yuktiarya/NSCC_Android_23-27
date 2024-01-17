727185a2b4c496d79e08da7e89612523a0578748
Rajat Jain <254jainrajat@gmail.com>
2024-01-17 18:45:23 +0530

    sign up project of rajat

---
+++ Project 2(Login)/rajat/sign_up_page/app/src/main/java/com/example/sign_up_page/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sign_up_page
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
